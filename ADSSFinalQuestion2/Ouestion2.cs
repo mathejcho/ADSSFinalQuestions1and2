eae825f0e09885283dbe57c56ceb3bb7acf4802b
matej petrovski <matej.pet@outlook.com>
2022-02-23 20:28:04 +0100

    Add project files.

---
+++ ADSSFinalQuestion2/Ouestion2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSSFinalQuestion2
+{
+    class Ouestion2
+    {
+
+        static void Main(string[] args)
+        {
+            Ouestion2 p = new Ouestion2();
+
+
+            string[] sheep = new string[6];
+
+            Console.WriteLine("Enter status of 6 sheep ");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Enter status  ");
+                sheep[i] = Console.ReadLine();
+            }
+
+            p.countSheep(sheep);
+            Console.ReadLine();
+        }
+
+        public void countSheep(string[] sheep)
+        {
+            int here = 0;
+            int nothere = 0;
+            int missing = 0;
+
+
+            foreach (string shep in sheep)
+            {
+                if (shep == "True" || shep == "true")
+                {
+
+                    here++;
+                }
+                else if (shep == "False" || shep == "false")
+                {
+
+                    nothere++;
+                }
+                else
+                {
+
+                    missing++;
+                }
+            }
+
+            Console.WriteLine(here + " sheep are present ");
+            Console.WriteLine(nothere + " sheep are absent ");
+            Console.WriteLine(missing + " sheep presence unknown");
+
+
+        }
+
+    }
+}
---
+++ ADSSFinalQuestion2/Question1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ADSSFinalQuestion1
+{
+    class Program
+    {
+        public void FindStudent()
+        {
+            Console.WriteLine("Enter string");
+            string st = Console.ReadLine();
+
+            string[] arr = st.Split(',');
+
+            int[] Students = Array.ConvertAll(arr, int.Parse);
+
+           
+
+            Array.Sort(Students);
+            int youngest = 0;
+            int oldest = 0;
+
+            for (int i = 0; i < Students.Length; i++)
+            {
+                if (Students[0] == Students[i])
+                {
+                    youngest = youngest + 1;
+                    
+                }
+            }
+
+            for (int y = 7; y > 0; y--)
+            {
+                if (Students[7] == Students[y])
+                {
+                    oldest = oldest + 1;
+                    
+                }
+            }
+
+
+            Console.WriteLine("the youngest Student is " + Students[0]);
+            if (youngest > 1)
+            {
+                Console.WriteLine("\nThere are " + youngest + " students at that age in the group. ");
+            }
+
+            Console.WriteLine("\nThe oldest student is" + Students[7]);
+
+            if (oldest > 1)
+            {
+                Console.WriteLine("\nThere are " + oldest + " students at that age in the group. ");
+            }
+
+
+
+
+
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 15, 9, 12, 15, 7, 7, 8 };
+
+            Program p = new Program();
+           
+            p.FindStudent();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
